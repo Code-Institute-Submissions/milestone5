6a5853bde98ba00a23f4105fa3188b5cc0918ac8
Phil Surgenor <philsurgenor87@gmail.com>
2018-12-15 17:57:43 +0000

    Edited Allowed Hosts

--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'ua_issue-tracker.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'ua-issue-tracker.herokuapp.com']
 
 
 # Application definition
