591f34fefc3a39267fa3aa762b91a8aeaf3882c2
Phil Surgenor <philsurgenor87@gmail.com>
2018-12-18 12:21:48 +0000

    Added login template to accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from accounts.views import logout
+from accounts.views import logout, login
 
 
 urlpatterns = [
     url(r'^logout/$', logout, name="logout"),
+    url(r'^login/$', login, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 
+
+"""
+Logs user out and returns them to the issue-tracker home page
+"""
+@login_required
 def logout(request):
-  """Log user out"""
   auth.logout(request)
   messages.success(request, "You have been logged out!")
   return redirect(reverse('tracker_home'))
+
+  
+
+"""
+Brings User to login page, if they submit valid form it
+logs user in and returns them to the issue-tracker home page
+""" 
+def login(request):
+  
+  return render(request, 'login.html')
