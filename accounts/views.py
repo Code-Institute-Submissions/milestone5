b7355970d7f81f6496d41f3f121e03f79bb4122e
Phil Surgenor <philsurgenor87@gmail.com>
2018-12-18 21:35:24 +0000

    Added Registration Forms and Profile Page

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
+from django.contrib.auth.models import User
 
 
 """
@@ -42,3 +43,40 @@ def login(request):
     login_form = UserLoginForm()
   
   return render(request, 'login.html', {"login_form": login_form})
+  
+  
+  
+
+"""
+REGISTRATION
+"""
+def register(request):
+  if request.user.is_authenticated:
+    return redirect(reverse('tracker_home'))
+    
+  if request.method == 'POST':
+    registration_form = UserRegistrationForm(request.POST)
+    
+    if registration_form.is_valid():
+      registration_form.save()
+      
+      user = auth.authenticate(username=request.POST['username'],
+                              password=request.POST['password1'])
+                              
+      if user:
+        auth.login(user=user, request=request)
+        messages.success(request, "Congratulations! You're One Of Us Now.")
+        return redirect(reverse('tracker_home'))
+      else: messages.error(request, "Unable to register your account at this time")
+                              
+  else:
+    registration_form = UserRegistrationForm()
+  return render(request, 'register.html', {"registration_form": registration_form})
+  
+  
+"""
+USER PROFILE
+"""
+def profile(request):
+  user = User.objects.get(email=request.user.email)
+  return render(request, 'profile.html', {"profile": user})
