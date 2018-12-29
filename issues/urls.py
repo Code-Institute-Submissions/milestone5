2f8409ae6bb71490cb571b8d59e712f7a1f8a088
Phil Surgenor <philsurgenor87@gmail.com>
2018-12-29 10:59:39 +0000

    fixed url for issue-tracker page

--- issues/urls.py
+++ issues/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from pages.views import issue_tracker
 
 urlpatterns = [
-      url(r'^$', issue_tracker, name='tracker_home'),  
+  url(r'^$', issue_tracker, name='tracker_home'),  
 ]
