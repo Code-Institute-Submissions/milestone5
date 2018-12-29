3bce299661cf1742b71a12617d0b28c8abff0d4e
Phil Surgenor <philsurgenor87@gmail.com>
2018-12-29 11:47:17 +0000

    Added Models for bugs and features

--- issues/admin.py
+++ issues/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from.models import Bug, Feature
 
-# Register your models here.
+admin.site.register(Bug)
+admin.site.register(Feature)
--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+class Bug(models.Model):
+    """
+    A Bug Report
+    """
+    title = models.CharField(max_length=200)
+    decsription = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    fixed = models.BooleanField(blank=False, default=False)
+    date_fixed = models.DateTimeField(blank=True, null=True)
+    score = models.IntegerField(default=0)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+        
+        
+
+class Feature(models.Model):
+    """
+    A Feature Suggestion
+    """
+    title = models.CharField(max_length=200)
+    decsription = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    added = models.BooleanField(blank=False, default=False)
+    date_added = models.DateTimeField(blank=True, null=True)
+    score = models.IntegerField(default=0)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
