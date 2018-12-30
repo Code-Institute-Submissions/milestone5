f3d9c9d37a62dd34cd03d3ba60a3382f4f3c8b9d
Phil Surgenor <philsurgenor87@gmail.com>
2018-12-30 23:29:08 +0000

    Added Charts to charts app

--- charts/urls.py
+++ charts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import view_charts
+from .views import view_charts, get_data
 
 urlpatterns = [
       url(r'^$', view_charts, name='charts'),  
+      url(r'^data/$', get_data, name='data'),  
 ]
--- charts/views.py
+++ charts/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from issues.models import Bug, Feature
+from datetime import date
+import datetime
 
 def view_charts(request):
   return render(request,'charts.html')
   
+
+def get_data(request):
+  fixed_bugs = Bug.objects.filter(fixed=True).order_by('-date_fixed')
+  total_fixed_today  = fixed_bugs.filter(date_fixed__date=date.today()).count()
   
+  this_month = datetime.datetime.now().month
+  total_fixed_month = Bug.objects.filter(date_fixed__month=this_month).count()
+  total_added_month  = Feature.objects.filter(date_added__month=this_month).count()
+  
+  this_year = datetime.datetime.now().year
+  total_fixed_year = Bug.objects.filter(date_fixed__year=this_year).count()
+  total_added_year  = Feature.objects.filter(date_added__year=this_year).count()
+  
+  added_features = Feature.objects.filter(added=True).order_by('-date_added')
+  total_added_today  = added_features.filter(date_added__date=date.today()).count()
+  
+  
+  
+  labels = ["Bugs", "Features"]
+  items = [total_fixed_today, total_added_today]
+  
+  itemsmonth = [total_fixed_month, total_added_month]
+  
+  itemsyear = [total_fixed_year, total_added_year]
+  
+  data = {
+    "labels":labels,
+    "items":items,
+    "itemsmonth":itemsmonth,
+    "itemsyear":itemsyear
+  }
+  
+  return JsonResponse(data)
