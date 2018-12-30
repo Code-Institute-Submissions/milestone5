85d6729be2ebf5e73510a47ca2e52d08beba2af4
Phil Surgenor <philsurgenor87@gmail.com>
2018-12-30 06:27:28 +0000

    Added tickets to issue tracker home and styled them

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from issues.models import Bug
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
     
 def issue_tracker(request):
-    return render(request, 'issuetracker.html')
+    fixed_bugs = Bug.objects.filter(fixed=True).order_by('-date_fixed')
+    total_fixed = fixed_bugs.count()
+    
+    working_bugs = Bug.objects.filter(working_on=True).order_by('-score')
+    total_working = working_bugs.count()
+    
+    other_bugs = Bug.objects.filter(working_on=False, fixed=False).order_by('-score')
+    total_other = other_bugs.count()
+    return render(request, 'issuetracker.html', {'fixed_bugs':fixed_bugs,
+                                                'total_fixed':total_fixed,
+                                                'working_bugs':working_bugs,
+                                                'total_working':total_working,
+                                                'other_bugs':other_bugs,
+                                                'total_other':total_other,
+                                                })
